a36cf839dccb017758f6b2870acc89b3b95b88be
Gwenhael Goavec-Merou <gwenhael@enjoy-digital.fr>
2025-03-12 15:25:01 +0100

    litecompute_poc/maia_hdl_fir_wrapper.py: switch to FIR4DSP

--- litecompute_poc/maia_hdl_fir_wrapper.py
+++ litecompute_poc/maia_hdl_fir_wrapper.py
@@ -19,33 +19,33 @@ from .clk_nx_common_edge import ClkNxCommonEdge
 # Generator ----------------------------------------------------------------------------------------
 
 def fir_generator(output_path,
-    data_in_width  = 12,
-    data_out_width = [16] * 3,
+    data_in_width  = 16,
+    data_out_width = 16,
     coeff_width    = 18,
-    decim_width    = [7, 6, 7],
-    oper_width     = [7, 6, 7],
-    macc_trunc     = [17, 18, 18],
+    decim_width    = 7,
+    oper_width     = 7,
+    macc_trunc     = 19,
+    len_log2       = 8, # Maximum FIR length is log2
     ):
 
     from amaranth.back.verilog import convert
     from maia_hdl.pluto_platform import PlutoPlatform
-    from maia_hdl.fir import FIRDecimator3Stage
-    fir = FIRDecimator3Stage(
+    from maia_hdl.fir import FIR4DSP
+    fir = FIR4DSP(
         in_width    = data_in_width,
         out_width   = data_out_width,
         coeff_width = coeff_width,
         decim_width = decim_width,
         oper_width  = oper_width,
         macc_trunc  = macc_trunc,
+        len_log2    = len_log2,
     )
 
     ports=[
         fir.coeff_waddr, fir.coeff_wren, fir.coeff_wdata,
-        fir.decimation1, fir.decimation2, fir.decimation3,
-        fir.bypass2, fir.bypass3,
-        fir.operations_minus_one1, fir.operations_minus_one2,
-        fir.operations_minus_one3,
-        fir.odd_operations1, fir.odd_operations3,
+        fir.decimation,
+        fir.operations_minus_one,
+        fir.odd_operations,
         fir.re_in, fir.im_in, fir.in_valid, fir.in_ready,
         fir.re_out, fir.im_out, fir.strobe_out
     ]
@@ -65,51 +65,43 @@ def fir_generator(output_path,
 
 class MAIAHDLFIRWrapper(LiteXModule):
     def __init__(self, platform,
-        data_in_width  = 12,
-        data_out_width = [16] * 3,
+        data_in_width  = 16,
+        data_out_width = 16,
         coeff_width    = 18,
-        decim_width    = [7, 6, 7],
-        oper_width     = [7, 6, 7],
-        macc_trunc     = [17, 18, 18],
+        decim_width    = 7,
+        oper_width     = 7,
+        macc_trunc     = 19,
+        len_log2       = 8,
         cd_domain      = "sys",
         add_csr        = True,
         ):
 
         # Streams ----------------------------------------------------------------------------------
-        self.sink   = sink          = stream.Endpoint([("data", 2 * data_in_width)])
-        self.source = source        = stream.Endpoint([("data", 2 * data_out_width[-1])])
+        self.sink   = sink        = stream.Endpoint([("data", 2 * data_in_width)])
+        self.source = source      = stream.Endpoint([("data", 2 * data_out_width)])
 
         # Parameters/Locals ------------------------------------------------------------------------
-        self.platform               = platform
-        self.data_in_width          = data_in_width
-        self.data_out_width         = data_out_width
-        self.coeff_width            = coeff_width
-        self.decim_width            = decim_width
-        self.oper_width             = oper_width
-        self.macc_trunc             = macc_trunc
-
-        # Bypass -----------------------------------------------------------------------------------
-        self.bypass2               = Signal()
-        self.bypass3               = Signal()
+        self.platform             = platform
+        self.data_in_width        = data_in_width
+        self.data_out_width       = data_out_width
+        self.coeff_width          = coeff_width
+        self.decim_width          = decim_width
+        self.oper_width           = oper_width
+        self.macc_trunc           = macc_trunc
+        self.len_log2             = len_log2
 
         # Decimation -------------------------------------------------------------------------------
-        self.decimation1           = Signal(decim_width[0])
-        self.decimation2           = Signal(decim_width[1])
-        self.decimation3           = Signal(decim_width[2])
-
+        self.decimation           = Signal(decim_width)
         # FIR Coefficient --------------------------------------------------------------------------
-        self.coeff_wren            = Signal()
-        self.coeff_waddr           = Signal(10)          # FIXME: size
-        self.coeff_wdata           = Signal(coeff_width)
+        self.coeff_wren           = Signal()
+        self.coeff_waddr          = Signal(len_log2)
+        self.coeff_wdata          = Signal(coeff_width)
 
         # Operations Minus One ---------------------------------------------------------------------
-        self.operations_minus_one1 = Signal(oper_width[0])
-        self.operations_minus_one2 = Signal(oper_width[1])
-        self.operations_minus_one3 = Signal(oper_width[2])
+        self.operations_minus_one = Signal(oper_width)
 
         # Odd Operations ---------------------------------------------------------------------------
-        self.odd_operations1       = Signal()
-        self.odd_operations3       = Signal()
+        self.odd_operations       = Signal()
 
         # # #
 
@@ -117,8 +109,8 @@ class MAIAHDLFIRWrapper(LiteXModule):
         # --------
         self.re_in        = Signal(data_in_width)
         self.im_in        = Signal(data_in_width)
-        self.re_out       = Signal(data_out_width[-1])
-        self.im_out       = Signal(data_out_width[-1])
+        self.re_out       = Signal(data_out_width)
+        self.im_out       = Signal(data_out_width)
         self.source_first = Signal()
 
         # FIR Instance -----------------------------------------------------------------------------
@@ -127,41 +119,33 @@ class MAIAHDLFIRWrapper(LiteXModule):
         self.ip_params = dict()
         self.ip_params.update(
             # Clk/Reset.
-            i_clk                   = ClockSignal(cd_domain),
-            i_rst                   = ResetSignal(cd_domain),
+            i_clk                  = ClockSignal(cd_domain),
+            i_rst                  = ResetSignal(cd_domain),
 
             # FIR Coefficient.
-            i_coeff_wren            = self.coeff_wren,
-            i_coeff_wdata           = self.coeff_wdata,
-            i_coeff_waddr           = self.coeff_waddr,
+            i_coeff_wren           = self.coeff_wren,
+            i_coeff_wdata          = self.coeff_wdata,
+            i_coeff_waddr          = self.coeff_waddr,
 
             # Decimation.
-            i_decimation1           = self.decimation1,
-            i_decimation2           = self.decimation2,
-            i_decimation3           = self.decimation3,
-
-            # Bypass.
-            i_bypass2               = self.bypass2,
-            i_bypass3               = self.bypass3,
+            i_decimation           = self.decimation,
 
             # Operations Minus One.
-            i_operations_minus_one1 = self.operations_minus_one1,
-            i_operations_minus_one2 = self.operations_minus_one2,
-            i_operations_minus_one3 = self.operations_minus_one3,
+            i_operations_minus_one = self.operations_minus_one,
 
-            i_odd_operations1       = self.odd_operations1,
-            i_odd_operations3       = self.odd_operations3,
+            # Number of Operations.
+            i_odd_operations       = self.odd_operations,
 
             # Input
-            i_re_in                 = self.re_in,
-            i_im_in                 = self.im_in,
-            i_in_valid              = sink.valid,
-            o_in_ready              = sink.ready,
+            i_re_in                = self.re_in,
+            i_im_in                = self.im_in,
+            i_in_valid             = sink.valid,
+            o_in_ready             = sink.ready,
 
             # Output
-            o_re_out                = self.re_out,
-            o_im_out                = self.im_out,
-            o_strobe_out            = source.valid,
+            o_re_out               = self.re_out,
+            o_im_out               = self.im_out,
+            o_strobe_out           = source.valid,
         )
 
         self.specials += Instance(self.ip_name, **self.ip_params)
@@ -182,32 +166,19 @@ class MAIAHDLFIRWrapper(LiteXModule):
 
     def add_csr(self):
         self._cfg = CSRStorage(description="Configuration Register.", fields=[
-            CSRField("bypass2",         size=1, offset=0,            description="Bypass FIR stage 2."),
-            CSRField("bypass3",         size=1, offset=1,            description="Bypass FIR stage 3."),
-            CSRField("odd_operations1", size=1, offset=4,            description="ODD Operation 1"),
-            CSRField("odd_operations3", size=1, offset=5,            description="ODD Operation 3"),
+            CSRField("odd_operations", size=1, offset=0,         description="ODD Operation"),
         ])
 
-        self._decimation1 = CSRStorage(self.decim_width[0],          description="Decimation factor for Stage1.")
-        self._decimation2 = CSRStorage(self.decim_width[1],          description="Decimation factor for Stage2.")
-        self._decimation3 = CSRStorage(self.decim_width[2],          description="Decimation factor for Stage3.")
+        self._decimation = CSRStorage(self.decim_width,          description="Decimation factor for Stage.")
 
-        self._coeff_waddr = CSRStorage(10,                           description="FIR Coefficient Address.") # FIXME: size
-        self._coeff_wdata = CSRStorage(self.coeff_width,             description="FIR Coefficient Data.")
+        self._coeff_waddr = CSRStorage(self.len_log2,            description="FIR Coefficient Address.")
+        self._coeff_wdata = CSRStorage(self.coeff_width,         description="FIR Coefficient Data.")
 
-        self._operations_minus_one1 = CSRStorage(self.oper_width[0], description="Operations Minus One Stage 1.")
-        self._operations_minus_one2 = CSRStorage(self.oper_width[1], description="Operations Minus One Stage 2.")
-        self._operations_minus_one3 = CSRStorage(self.oper_width[2], description="Operations Minus One Stage 3.")
+        self._operations_minus_one = CSRStorage(self.oper_width, description="Operations Minus One Stage.")
 
         self.comb += [
-            # Bypass.
-            self.bypass2.eq(self._cfg.fields.bypass2),
-            self.bypass3.eq(self._cfg.fields.bypass3),
-
             # Decimations.
-            self.decimation1.eq(self._decimation1.storage),
-            self.decimation2.eq(self._decimation2.storage),
-            self.decimation3.eq(self._decimation3.storage),
+            self.decimation.eq(self._decimation.storage),
 
             # Coeff configuration.
             self.coeff_wren.eq(self._coeff_wdata.re),
@@ -215,13 +186,10 @@ class MAIAHDLFIRWrapper(LiteXModule):
             self.coeff_wdata.eq(self._coeff_wdata.storage),
 
             # Operations minus one.
-            self.operations_minus_one1.eq(self._operations_minus_one1.storage),
-            self.operations_minus_one2.eq(self._operations_minus_one2.storage),
-            self.operations_minus_one3.eq(self._operations_minus_one3.storage),
+            self.operations_minus_one.eq(self._operations_minus_one.storage),
 
             # ODD Operations.
-            self.odd_operations1.eq(self._cfg.fields.odd_operations1),
-            self.odd_operations3.eq(self._cfg.fields.odd_operations3),
+            self.odd_operations.eq(self._cfg.fields.odd_operations),
         ]
 
     def do_finalize(self):
@@ -239,6 +207,7 @@ class MAIAHDLFIRWrapper(LiteXModule):
             decim_width    = self.decim_width,
             oper_width     = self.oper_width,
             macc_trunc     = self.macc_trunc,
+            len_log2       = self.len_log2,
         )
 
         self.platform.add_source(v_file)
