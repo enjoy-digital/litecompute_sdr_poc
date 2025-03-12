2893906d75e60efa570630dcc80dcfa6f242ae03
Gwenhael Goavec-Merou <gwenhael@enjoy-digital.fr>
2025-03-12 12:55:34 +0100

    software/user/display_fft.py: added argument to specify input file, sample frequency

--- software/user/display_fft.py
+++ software/user/display_fft.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 
+#
+# This file is part of LiteCompute PoC project.
+#
+# Copyright (c) 2025 Enjoy-Digital <enjoy-digital.fr>.
+#
+# SPDX-License-Identifier: BSD-2-Clause
+
+import sys
+import argparse
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -15,43 +25,50 @@ def read_binary_file(file_path):
             samples.append(value)
     return samples
 
+# Main ---------------------------------------------------------------------------------------------
 
-# Parameters
-f       = 25e6 / 1 # Signal frequency
-fs      = 100e6    # Sampling frequency
+def main():
+    parser = argparse.ArgumentParser(description="Display FFT.")
+    parser.add_argument("--dump-file",                            help="litepcie_test record result file dump.")
+    parser.add_argument("--fft-order", default=32,    type=int,   help="FFT Order.")
+    parser.add_argument("--fs",        default=100e6, type=float, help="Sample Frequency.")
 
-re_in   = []
-im_in   = []
-last_in = []
-N       = 32
+    args = parser.parse_args()
 
-samples = read_binary_file("toto.bin")
+    # Parameters
+    f       = 25e6 / 1 # Signal frequency
+    fs      = args.fs  # Sampling frequency
 
-for i in range(0, len(samples), 2):
-    re_in.append(samples[i + 0])
-    im_in.append(samples[i + 1])
+    re_in   = []
+    im_in   = []
+    last_in = []
+    N       = args.fft_order
 
-# Frequency axis.
-freq_raw = np.arange(N) * (fs/2 / N)
-#freq_raw = freq_raw[0:N // 2]
+    samples = read_binary_file(args.dump_file)
 
+    for i in range(0, len(samples), 2):
+        re_in.append(samples[i + 0])
+        im_in.append(samples[i + 1])
 
-plt.figure(figsize=(12, 6))
-for i in range(1*N, len(re_in)-N, N):
-    real     = np.array(re_in[i: i + N])
-    imag     = np.array(im_in[i: i + N])
-    fft_data = real + 1j * imag
-    # Magnitude of FFT
-    magnitude = np.abs(fft_data)
-    #magnitude = magnitude[0:N // 2]
+    # Frequency axis.
+    freq_raw = np.arange(N) * (fs/2 / N)
 
-    # Plot
-    plt.plot(freq_raw / 1e6, magnitude, '-o', markersize=4)#, label='FFT Magnitude')
-plt.xlabel('Frequency (MHz)')
-plt.ylabel('Magnitude')
-plt.grid(True)
+    plt.figure(figsize=(12, 6))
+    for i in range(1*N, len(re_in)-N, N):
+        real     = np.array(re_in[i: i + N])
+        imag     = np.array(im_in[i: i + N])
+        fft_data = real + 1j * imag
 
-plt.axvline(x=f / 1e6, color='g', linestyle='--', label=f'Signal ({f} MHz)')
-plt.legend()
+        # Magnitude of FFT
+        magnitude = np.abs(fft_data)
 
-plt.show()
+        # Plot
+        plt.plot(freq_raw / 1e6, magnitude, '-o', markersize=4)
+    plt.xlabel('Frequency (MHz)')
+    plt.ylabel('Magnitude')
+    plt.grid(True)
+
+    plt.axvline(x=f / 1e6, color='g', linestyle='--', label=f'Signal ({f} MHz)')
+    plt.legend()
+
+    plt.show()
