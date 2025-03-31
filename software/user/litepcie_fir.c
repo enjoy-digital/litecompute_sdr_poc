914ed22145fab3997677a706907baa79091bf369
Gwenhael Goavec-Merou <gwenhael@enjoy-digital.fr>
2025-03-31 11:22:33 +0200

    software/user/litepcie_fir.c: added cmd to configure FIR (operations/decimation)

--- software/user/litepcie_fir.c
+++ software/user/litepcie_fir.c
@@ -25,6 +25,7 @@ static char litepcie_device[1024];
 static int litepcie_device_num;
 
 /* FIR Coefficients */
+/*------------------*/
 
 static void fir_coefficients_write(const char *filename)
 {
@@ -40,7 +41,7 @@ static void fir_coefficients_write(const char *filename)
     }
 
     printf("\e[1m[> Fir Coefficients Configuration:\e[0m\n");
-    printf("-------------------------\n");
+    printf("----------------------------------\n");
 
     fd_coefficients = fopen(filename, "r");
     if (!fd_coefficients) {
@@ -85,20 +86,52 @@ static void fir_coefficients_write(const char *filename)
     close(fd);
 }
 
+/* Fir Parameters configuration */
+/*------------------------------*/
+
+static void fir_configuration(uint32_t decimation, uint32_t operations, uint8_t odd_operations)
+{
+    int fd;
+
+    printf("\e[1m[> Fir Parameters Configuration:\e[0m\n");
+    printf("--------------------------------\n");
+
+    fd = open(litepcie_device, O_RDWR);
+    if (fd < 0) {
+        fprintf(stderr, "Could not init driver %s\n", litepcie_device);
+        exit(1);
+    }
+
+    /* write decimation. */
+    litepcie_writel(fd, CSR_FIR_DECIMATION_ADDR, decimation);
+
+    /* write operations (Minus one). */
+    litepcie_writel(fd, CSR_FIR_OPERATIONS_MINUS_ONE_ADDR, operations - 1);
+
+    /* write odd/event operations. */
+    litepcie_writel(fd, CSR_FIR_CFG_ADDR, (odd_operations & 0x01) << CSR_FIR_CFG_ODD_OPERATIONS_OFFSET);
+
+    close(fd);
+}
+
 /* Help */
 /*------*/
 
 static void help(void)
 {
-    printf("LitePCIe utilities\n"
-           "usage: litepcie_util [options] cmd [args...]\n"
+    printf("LitePCIe Fir Utility\n"
+           "usage: litepcie_fir [options] cmd [args...]\n"
            "\n"
            "options:\n"
-           "-h                                Help.\n"
-           "-c device_num                     Select the device (default = 0).\n"
+           "-h                    Help.\n"
+           "-c device_num         Select the device (default = 0).\n"
+           "-d decimation         Select decimation factor (default = 2).\n"
+           "-o operations         Select number operations to performs (default = 4).\n"
+           "-O odd_operations     Select if operations is odd or eveen (default = 0).\n"
            "\n"
            "available commands:\n"
-           "coefficients filename             FIR Coefficients Configuration from file.\n"
+           "coefficients filename FIR Coefficients Configuration from file.\n"
+           "configuration         FIR Parameter Configuration.\n"
            "\n"
            );
     exit(1);
@@ -113,15 +146,30 @@ int main(int argc, char **argv)
     int c;
 
     litepcie_device_num = 0;
+    uint32_t decimation = 2;
+    uint32_t operations = 4;
+    uint8_t odd_operations = 0;
 
     /* Parameters. */
     for (;;) {
-        c = getopt(argc, argv, "hc:f:");
+        c = getopt(argc, argv, "hc:d::o:O");
         if (c == -1)
             break;
         switch(c) {
         case 'h':
             help();
+            break;
+        case 'c':
+            litepcie_device_num = atoi(optarg);
+            break;
+        case 'd':
+            decimation = atoi(optarg);
+            break;
+        case 'o':
+            operations = atoi(optarg);
+            break;
+        case 'O':
+            odd_operations = atoi(optarg);
             break;
         default:
             exit(1);
@@ -137,18 +185,20 @@ int main(int argc, char **argv)
 
     cmd = argv[optind++];
 
-    /* Fir Coefficients configuration */
+    /* Fir Coefficients configuration. */
     if (!strcmp(cmd, "coefficients")) {
+        printf("hello %d %d\n", optind, argc);
         const char *filename = NULL;
-        if (optind != argc) {
+        if (optind + 1 > argc) {
             goto show_help;
         }
         filename = argv[optind++];
         fir_coefficients_write(filename);
-    }
-
-    return 0;
-
+    /* Fir Parameters configuration. */
+    } else if (!strcmp(cmd, "configuration")) {
+        fir_configuration(decimation, operations, odd_operations);
+    /* Show help otherwise. */
+    } else
 show_help:
         help();
 
